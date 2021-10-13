a3219d341195b27a7a598bdd69ba97880dc163fb
krisrod10 <82917336+krisrod10@users.noreply.github.com>
2021-10-12 21:10:22 -0500

    heroku test

--- controller/users.js
+++ controller/users.js
@@ -29,7 +29,7 @@ let createUser = function (req, res) {
     params.push(email);
     params.push(fullname);
     params.push(password);
-    // video paused at 1:27:20
+    
 
      db.query(sql, params, function (error, rows) {
          if(error) {
@@ -43,28 +43,39 @@ let createUser = function (req, res) {
 
 
 let listUsers = function (req, res) {
-    console.log("GET /users: ", req);
+    console.log("GET /users: ", req); // using this to make sure the connection works
 res.json("Inside place holder");   
+mysqlConnection.query("SELECT * FROM users2", (error, rows, fields) => { // using the mysql connection to talk to the database to retrieve all info from users2
+if(!error){ // if there is not a error then we send back the rows to get the information from the users
+        res.send(rows)
+    } else{
+        console.log(error) // if not we want to see what error is coming back inside of the console
+    }
+})
+
 }
 
+
 let deleteUsers = function (req,res) {
-    let users = [];
+    // using the place holder to make sure the connection works
     console.log("INSIDE delete");
     res.json("Place Holder");
-    const { id } = req.params;
+    const { id } = req.params; //requsting the data 
 
-    const deleted = users.find(users => users.id === id);
-    if(deleted){
+    const deleted = users.find(users => users.id === id); // find the user id that is equal to the id searched
+    if(deleted){ // then use the filter method to get that id anf filter it out if that id is == null
    users = users.filter(users => users.id <= id);
 
     } else{
-        res.status(204).json("User you are looking for does not exist")
+        res.status(204).json("User you are looking for does not exist") // always send me a status to let the user know what is going on
     }
 }
 
 let putUsers = function(req,res){
    console.log("INSIDE PUT");
    res.json("PLACE HOLDER");
+   
+   }
     
-}
+
 module.exports = { createUser, listUsers , deleteUsers, putUsers};
