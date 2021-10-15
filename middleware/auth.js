ce812d2aed4bc45969cda3ebaeb1f6bbcb1a6816
krisrod10 <82917336+krisrod10@users.noreply.github.com>
2021-10-14 19:30:41 -0500

    update

---
+++ middleware/auth.js
@@ -0,0 +1,32 @@
+let express = require("express");
+
+let db = require("../db/db");
+
+
+let loginCheck = function(req,res){
+    console.log("Inside login check");
+    let sql = "SELECT username, password WHERE username = ?;";
+    
+
+    let jwtSecret = 
+    
+    let username = req.body.username;
+    let password = req.body.password;
+
+    let params = [];
+    params.push(username);
+    params.push(password);
+
+    db.query(sql, params, function(error, rows){
+        if(error){
+            console.log("Wrong username or password");
+            res.sendStatus(204);
+        } else{
+            console.log("That username and password worked");
+            res.sendStatus(204);
+        }
+    })
+
+}
+
+module.exports= {loginCheck};
--- router/auth.js
+++ router/auth.js
@@ -3,6 +3,7 @@ let express = require("express"); // this is a express app
 // i want to use router
 let router = express.Router();
 
+let midWare = require("../middleware/auth");
 // the controller to link our functions
 let controller = require("../controller/auth");
 
@@ -10,10 +11,10 @@ let controller = require("../controller/auth");
 router.get("/everyone",controller.everyone);
  
 // route for a get to check the jwt token
-router.get("/authOnly", controller.checkJwt);
+router.get("/authOnly", [midWare.loginCheck] , controller.checkJwt);
 
 // route for a login that is producing a login
-router.post("/login", controller.login);
+router.post("/login",  controller.login);
 
 
 // a POST request to create a new user
